d6f7fa78c7a25f7d961f90721a9910d2d62b8272
Bhawesh <bhaweshjoshi689@gmail.com>
2026-06-05 00:38:14 +0530

    feat : make the ui of the microservice

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
+from requests import request
 import os
 import json
+import requests
+def fetch_categories():
+    try:
+        response = requests.get("http://localhost:8080/api/categories", timeout=5)
+        if response.status_code==200:
+            return response.json()
+    except Exception as e:
+        print(f"Cant fetch from the api fallback to local json file")
+    return load_json_config(filepath = 'categories.json ' , default_data={"Uncategorized" : 9})
 def load_json_config(filepath : str , default_data : dict)-> dict:
 
     if os.path.exists(filepath):
@@ -11,7 +21,6 @@ def load_json_config(filepath : str , default_data : dict)-> dict:
     else:
         print("file not found using defaults")
     return default_data
-    
-CATEGORY_ID_MAP = load_json_config(filepath='categories.json', default_data={"Uncategorized" : 9})
+CATEGORY_ID_MAP = fetch_categories()
+CATEGORY_POOL = list(CATEGORY_ID_MAP.keys())   
 KNOWN_MERCHANTS = load_json_config(filepath='known_merchants.json', default_data={})
-CATEGORY_POOL = list(CATEGORY_ID_MAP.keys())
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import io
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from pypdf import PdfReader
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
+
 
 # Import from your new custom modules
 from config import CATEGORY_ID_MAP
@@ -8,7 +13,11 @@ from pre_processor import extract_phonepe_data, pre_process_merchant
 from classifier import classify_merchant_local
 
 app = FastAPI(title="PhonePe AI Parser (Production Edition)")
-
+templates = Jinja2Templates(directory="templates")
+@app.get("/upload", response_class=HTMLResponse)
+async def upload_page(request: Request, userId: str = None):
+    # Renders the HTML page and passes the userId into the frontend
+    return templates.TemplateResponse(request=request, name="index.html", context={"userId": userId})
 @app.post("/parser/process")
 async def process_statement(file: UploadFile = File(...)):
     if not file.filename.endswith('.pdf'):
